5e88b5e2238344a8bcc62d48dd8a61fe132eff17
Leonam Santos <leonam.ricardo@gmail.com>
2022-05-01 12:10:31 +0200

    Add task-manager

---
+++ src/task-manager/task-manager.ts
@@ -0,0 +1,71 @@
+/*
+Task Manager
+
+We need to handle multiple (potentially) long-running tasks.
+
+Complete the implementation of the "TaskManager" class, considering that:
+
+* If the number of tasks executing right now is less than the concurrency limit, tasks should be executed immediately;
+* If the number of tasks executing right now equals the concurrency limit, new taks should be queued and executed as soon as possible.
+
+*/
+
+/***** Implementatation *****/
+
+export type Task<TaskResultType> =
+    | (() => PromiseLike<TaskResultType>)
+    | (() => TaskResultType);
+
+export interface Options {
+    readonly concurrency?: number;
+}
+
+export class TaskManager {
+    private readonly queue: Task<unknown>[] = [];
+    private readonly _concurrency!: number;
+    private _pendingCount: number = 0;
+
+    public constructor(options?: Options) {
+        this._concurrency = options?.concurrency ?? Number.POSITIVE_INFINITY;
+    }
+
+    public get concurrency(): number {
+        return this._concurrency;
+    }
+
+    public get doesConcurrentAllowAnother(): boolean {
+        return this._pendingCount < this._concurrency;
+    }
+
+    public add<TaskResultType>(fn: Task<TaskResultType>): Promise<TaskResultType> {
+        return new Promise<TaskResultType>((resolve, reject) => {
+            const run = async (): Promise<void> => {
+                this._pendingCount++;
+
+                try {
+                    const result = await fn();
+                    resolve(result);
+                } catch (error: unknown) {
+                    reject(error);
+                }
+
+                this._pendingCount--;
+                this.tryToExecute();
+            };
+
+            this.queue.push(run);
+            this.tryToExecute();
+        });
+    }
+
+    public tryToExecute(): void {
+        if (!this.doesConcurrentAllowAnother) {
+            return;
+        }
+
+        const task = this.queue.shift();
+        if (task) {
+            task();
+        }
+    }
+}
---
+++ test/task-manager/task-manager.spec.ts
@@ -0,0 +1,74 @@
+import {TaskManager} from "../../src/task-manager/task-manager";
+
+describe(TaskManager.name, () => {
+    describe(".concurrency", () => {
+        [
+            {
+                options: undefined,
+                expected: Number.POSITIVE_INFINITY,
+            },
+            {
+                options: {},
+                expected: Number.POSITIVE_INFINITY,
+            },
+            {
+                options: { concurrency: 10 },
+                expected: 10,
+            },
+        ].forEach(({options, expected}, index) => {
+            it(`#${index}`, () => {
+                expect(new TaskManager(options).concurrency).toBe(expected);
+            });
+        });
+    });
+
+
+    describe(".doesConcurrentAllowAnother", () => {
+        const taskManager = new TaskManager({concurrency: 2});
+
+        it("SHOULD allow another", async () => {
+            Array.from({length: taskManager.concurrency - 1}).map(() => taskManager.add(() => {}));
+            expect(taskManager.doesConcurrentAllowAnother).toBeTruthy();
+        });
+
+        it("SHOULD NOT allow another", () => {
+            Array.from({length: taskManager.concurrency}).map(() => taskManager.add(() => {}));
+            expect(taskManager.doesConcurrentAllowAnother).toBeFalsy();
+        });
+    });
+
+    describe(".add()", () => {
+        const taskManager = new TaskManager({concurrency: 5});
+
+        it("SHOULD resolve WHEN added 1 task AND it resolves", async () => {
+            expect.assertions(1);
+            const fixture = Symbol("fixture");
+            await expect(taskManager.add(() => fixture)).resolves.toBe(fixture);
+        });
+
+        it("SHOULD reject WHEN added 1 task AND it throws", async () => {
+            expect.assertions(1);
+            const error = new Error("fixture");
+            await expect(taskManager.add(() => {throw error})).rejects.toBe(error);
+        });
+
+        it("SHOULD limit the number of tasks running WHEN the limit is less then number of tasks", async () => {
+            const length = taskManager.concurrency * 3;
+            expect.assertions(length);
+
+            let running = 0;
+            const promises = Array.from({length}).map(() => taskManager.add(async () => {
+                running++;
+                expect(running <= taskManager.concurrency).toBeTruthy();
+                await delay(randomInt(10, 20));
+                running--;
+            }));
+
+            await Promise.all(promises);
+        });
+    });
+});
+
+// HELPERS
+const delay = (time: number) => new Promise((res) => setTimeout(res, time));
+const randomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
