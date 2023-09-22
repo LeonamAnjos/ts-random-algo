aae1f90e62da2f6375ad3390fc791c57dc61e947
Leonam Santos <leonam.ricardo@gmail.com>
2023-09-22 21:16:24 +1000

    Fix taskTimeout typing

--- src/task-manager/task-timeout.ts
+++ src/task-manager/task-timeout.ts
@@ -1,43 +1,46 @@
 export class TimeoutError extends Error {
-    constructor(message?: string) {
-        super(message);
-        this.name = "TimeoutError";
-    }
+  constructor(message?: string) {
+    super(message);
+    this.name = "TimeoutError";
+  }
 }
 
-export const taskTimeout = <TaskResultType>(fn: Promise<TaskResultType>, ms?: number): Promise<TaskResultType> => {
-    if (!ms || ms === Number.POSITIVE_INFINITY) {
-        return fn;
+export const taskTimeout = <TaskResultType>(
+  fn: Promise<TaskResultType>,
+  ms?: number
+): Promise<TaskResultType> => {
+  if (!ms || ms === Number.POSITIVE_INFINITY) {
+    return fn;
+  }
+
+  let timer: any = null;
+
+  const clean = () => {
+    if (!!timer) {
+      clearTimeout(timer);
+      timer = null;
     }
+  };
 
-    let timer: NodeJS.Timeout | undefined = undefined;
+  return new Promise<TaskResultType>((resolve, reject) => {
+    timer = setTimeout(() => {
+      clean();
+      reject(new TimeoutError());
+    }, ms);
 
-    const clean = () => {
+    (async () => {
+      try {
+        const result = await fn;
         if (!!timer) {
-            clearTimeout(timer);
-            timer = undefined;
+          resolve(result);
         }
-    }
-
-    return new Promise<TaskResultType>((resolve, reject) => {
-        timer = setTimeout(() => {
-            clean();
-            reject(new TimeoutError());
-        }, ms);
-
-        (async () => {
-            try {
-                const result = await fn;
-                if (!!timer) {
-                    resolve(result);
-                }
-            } catch (error: unknown) {
-                if (!!timer) {
-                    reject(error);
-                }
-            } finally {
-                clean();
-            }
-        })();
-    });
-}
+      } catch (error: unknown) {
+        if (!!timer) {
+          reject(error);
+        }
+      } finally {
+        clean();
+      }
+    })();
+  });
+};
