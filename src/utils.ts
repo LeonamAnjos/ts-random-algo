0b5ced26c98773e09e6d00a521a352b9b01b9663
Leonam Santos <leonam.ricardo@gmail.com>
2023-09-25 23:09:48 +1000

    Add range method to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 export const randomInt = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
+
+export function* range(start: number, end: number) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
